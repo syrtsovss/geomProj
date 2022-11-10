164c75d6a33943ae96df827df5f9f3033c4f6e5b
Сырцов Степан Сергеевич <syrtsovss@mail.ru>
2022-11-10 09:36:32 +0300

    Files

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <imgui.h>
 #include <cmath>
 #include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
 
 const int SIZE_X=1080;
 const int SIZE_Y=720;
@@ -21,10 +24,13 @@ void setColor(float * newColor){
 // Прямоугольник
 struct Rect{
     //Вершины
-    sf::Vector2i pointA;
-    sf::Vector2i pointB;
+    sf::Vector2i pointA, pointB;
     //Точка на стороне
     sf::Vector2i pointP;
+    //Флаг, рассчитан ли прямоугольник
+    bool isSolved=false;
+    //Оставшиеся вершины
+    sf::Vector2f pointC={0, 0}, pointD={0, 0};
     Rect(const sf::Vector2i &pointA, const sf::Vector2i &pointB, const sf::Vector2i &pointP) : 
         pointA(pointA), pointB(pointB), pointP(pointP){}
 };
@@ -38,6 +44,9 @@ int nextRect[3][2]={{0, 0}, {0, 0}, {0, 0}};
 char counter=0;
 //Количество созданных случайных прямоугольников
 int nextRandom=10;
+//Пути к файлу для чтения и записи
+std::string pathIn="/home/m/in.txt";
+std::string pathOut="/home/m/out.txt";
 
 //Операции с векторами
 
@@ -56,6 +65,32 @@ sf::Vector2f turn(sf::Vector2f a){
     return sf::Vector2f(-a.y, a.x);
 }
 
+//Расчёт вершин прямоугольника
+void solveRect(Rect * rect){
+    //Вектор AD
+    sf::Vector2f AD;
+    /*Если точки A и B совпадают, 
+    то прямоугольник не определён корректно
+    Будем считать, что он совпадает с отрезком AP*/
+    if (rect->pointA==rect->pointB){
+        AD=static_cast<sf::Vector2f>(rect->pointP-rect->pointA);
+    }
+    //Если A и B не совпадают
+    else{
+        //Вычисляем вектор вдоль стороны и от вершины до точки на стороне
+        sf::Vector2f AB=static_cast<sf::Vector2f>(rect->pointB-rect->pointA);
+        sf::Vector2f AP=static_cast<sf::Vector2f>(rect->pointP-rect->pointA);
+        //Проецируем AP на прямую, перпендикулярную AB
+        sf::Vector2f orthogonal=norm(turn(AB));
+        float lenthAD=orthogonal*AP;
+        //Вычисляем AD
+        AD=lenthAD*orthogonal;
+    }
+    //Вычисляем оставшиеся вершины прямоугольника
+    rect->pointD=static_cast<sf::Vector2f>(rect->pointA)+AD;
+    rect->pointC=static_cast<sf::Vector2f>(rect->pointB)+AD;
+}
+
 //Рисуем задачу
 void render(sf::Vector2u size){
     // Задаём размеры и положение невидимого окна
@@ -68,17 +103,11 @@ void render(sf::Vector2u size){
     //Получаем список примитивов
     auto drawList=ImGui::GetWindowDrawList();
     for (auto rect: rectangles){
-        //Вычисляем вектор вдоль стороны и от вершины до точки на стороне
-        sf::Vector2f AB=static_cast<sf::Vector2f>(rect.pointB-rect.pointA);
-        sf::Vector2f AP=static_cast<sf::Vector2f>(rect.pointP-rect.pointA);
-        //Проецируем AP на прямую, перпендикулярную AB
-        sf::Vector2f orthogonal=norm(turn(AB));
-        float lenthAD=orthogonal*AP;
-        //Вычисляем AD
-        sf::Vector2f AD=lenthAD*orthogonal;
-        //Вычисляем оставшиеся вершины треугольника
-        sf::Vector2i pointD=rect.pointA+static_cast<sf::Vector2i>(AD);
-        sf::Vector2i pointC=rect.pointB+static_cast<sf::Vector2i>(AD);
+        if(!rect.isSolved){
+            solveRect(&rect);
+        }
+        sf::Vector2i pointD=static_cast<sf::Vector2i>(rect.pointD);
+        sf::Vector2i pointC=static_cast<sf::Vector2i>(rect.pointC);
 
         //Рисуем стороны прямоугольника
         drawList->AddLine(
@@ -182,6 +211,64 @@ void showRandom(sf::RenderWindow * windowPtr){
         addRandom(nextRandom, windowPtr);
     }
 }
+//Запись в файл
+void writeToFile(){
+    //Открываем файл
+    std::ofstream file;
+    file.open(pathOut);
+    //Если файл не открывается
+    if(!file){
+        std::cerr << "Can't open file";
+        return;
+    }
+    //Для каждого прямоугольника
+    for(auto rect: rectangles){
+        //Записываем в файл координаты точек прямоугольника
+        file << rect.pointA.x << ' ' << rect.pointA.y << ' ';
+        file << rect.pointB.x << ' ' << rect.pointB.y << ' ';
+        file << rect.pointP.x << ' ' << rect.pointP.y << '\n';
+    }
+    //Закрываем файл
+    file.close();
+}
+
+//Чтение из файла
+void readFromFile(){
+    //Открываем файл
+    std::ifstream file;
+    file.open(pathIn);
+    //Если файл не открывается
+    if(!file){
+        std::cerr << "Can't open file";
+        return;
+    }
+    //Очищаем вектор прямоугольников
+    rectangles.clear();
+    //Переменные для чтения из файла
+    int pointAX,pointAY,pointBX,pointBY,pointPX,pointPY;
+    //Пока не конец файла
+    while(file >> pointAX >> pointAY >> 
+        pointBX >> pointBY >> 
+        pointPX >> pointPY){
+        //Создаём прямоугольник
+        rectangles.push_back(Rect(
+        sf::Vector2i(pointAX, pointAY),
+        sf::Vector2i(pointBX, pointBY),
+        sf::Vector2i(pointPX, pointPY)));
+    }
+    file.close();
+}
+
+void showFile(){
+    if(!ImGui::CollapsingHeader("Files"))
+        return;
+    if(ImGui::Button("Load"))
+        readFromFile();
+    ImGui::SameLine();
+    if(ImGui::Button("Save"))
+        writeToFile();
+}
+
 
 int main() {
     // Создаём окно
@@ -208,8 +295,8 @@ int main() {
             // Если нажато Ctrl+Q, закрываем окно
             if (event.type == sf::Event::KeyPressed && event.key.code==sf::Keyboard::Q && event.key.control)
                 window.close();
-            //Если событие - клик мыши
-            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button==sf::Mouse::Button::Left){
+            //Если событие - клик мыши, и оно не обработано элементами ImGui
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button==sf::Mouse::Button::Left && !ImGui::GetIO().WantCaptureMouse){
                 //Получаем координаты
                 nextRect[counter][0]=event.mouseButton.x;
                 nextRect[counter][1]=event.mouseButton.y;
@@ -234,8 +321,10 @@ int main() {
         backgroundSettings();
         // Добавление прямоугольника
         showRect();
-        // добавление случайных прямоугольников
+        // Добавление случайных прямоугольников
         showRandom(&window);
+        //Работа с файлами
+        showFile();
         // Заканчиваем рисовать окно
         ImGui::End();
         // Очищаем окно
